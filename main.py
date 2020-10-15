52461c54d4d214b84baf36eaf07524eaa7c5ebac
0x0bloodyknight <0x0bloodyknight@gmail.com>
2020-10-16 02:07:40 +0300

    Update main.py
    Transcription of message.reply_to_message, fixed typo

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=["ipa"])
 def handle_ipa(message):
     bot.send_chat_action(message.chat.id, "typing")
-    transcription = get_transcription(message.text)
+    transcription = ""
+    if hasattr(message.reply_to_message, "text") and len(message.text) == 4:
+        transcription = get_transcription(message.reply_to_message.text)
+    else:
+        transcription = get_transcription(message.text)
     bot.send_chat_action(message.chat.id, "typing")
     if transcription != "":
         bot.reply_to(message, text=transcription)
     else:
-        bot.reply_to(message, "Transcription unavailabe")
+        bot.reply_to(message, "Transcription unavailable")
 
 
 @bot.message_handler(func=lambda message: message.chat.type == "private")
@@ -26,7 +30,7 @@ def handle_private_chat(message):
     if transcription != "":
         bot.send_message(message.chat.id, text=transcription)
     else:
-        bot.reply_to(message, "Transcription unavailabe")
+        bot.reply_to(message, "Transcription unavailable")
 
 
 bot.polling()
