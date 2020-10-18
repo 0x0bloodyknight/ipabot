b17d0f14cf5ba993a99a6f29e95d02770656cdcf
0x0bloodyknight <0x0bloodyknight@gmail.com>
2020-10-18 03:16:30 +0300

    Added inline handler, removed unnesessary action sendings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 import config
 from tpparser import get_transcription
+from telebot import types
 
 API_TOKEN = config.token
 
@@ -15,7 +16,6 @@ def handle_ipa(message):
         transcription = get_transcription(message.reply_to_message.text)
     else:
         transcription = get_transcription(message.text)
-    bot.send_chat_action(message.chat.id, "typing")
     if transcription != "":
         bot.reply_to(message, text=transcription)
     else:
@@ -25,12 +25,30 @@ def handle_ipa(message):
 @bot.message_handler(func=lambda message: message.chat.type == "private")
 def handle_private_chat(message):
     bot.send_chat_action(message.chat.id, "typing")
+    transcription = ""
     transcription = get_transcription(message.text)
-    bot.send_chat_action(message.chat.id, "typing")
     if transcription != "":
         bot.send_message(message.chat.id, text=transcription)
     else:
         bot.reply_to(message, "Transcription unavailable")
+
+
+@bot.inline_handler(lambda query: query.query.isascii() == True)
+def query_text(inline_query):
+    transcription = ""
+    transcription = get_transcription(inline_query.query)
+    if transcription != "":
+        res = types.InlineQueryResultArticle(
+            "1", transcription, types.InputTextMessageContent(transcription)
+        )
+        bot.answer_inline_query(inline_query.id, [res])
+    else:
+        res = types.InlineQueryResultArticle(
+            "1",
+            inline_query.query,
+            types.InputTextMessageContent(inline_query.query),
+        )
+    bot.answer_inline_query(inline_query.id, [res])
 
 
 bot.polling()
